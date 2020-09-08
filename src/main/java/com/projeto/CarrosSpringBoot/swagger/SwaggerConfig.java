a099e7106d04b1ae687eac0606d331315b3940bc
harrisonmk <harrison.mitchell@hotmail.com>
2020-09-08 00:56:12 -0400

    configuracao swagger

--- src/main/java/com/projeto/CarrosSpringBoot/swagger/SwaggerConfig.java
+++ src/main/java/com/projeto/CarrosSpringBoot/swagger/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.projeto.CarrosSpringBoot.swagger;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -23,6 +26,16 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
+                .globalOperationParameters(
+                        Collections.singletonList(
+                                new ParameterBuilder()
+                                        .name("Authorization")
+                                        .description("Bearer token")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build())
+                )
                 .apiInfo(apiInfo());
     }
 
