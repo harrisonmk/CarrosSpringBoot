2610ac1b7e4b3a26c117bb0f5af59d80923bb7d3
Harrisonmk <harrison.mitchell@hotmail.com>
2020-09-14 11:37:32 -0400

    alterado um nome que estava errado

--- src/test/java/resources/data.sql
+++ src/test/java/resources/data.sql
@@ -3,7 +3,7 @@ insert into role(id,nome) values (2, 'ROLE_ADMIN');
 
 insert into usuario(id,nome,email,login,senha) values (1,'Ricardo Lecheta','rlecheta@gmail.com','rlecheta','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
 insert into usuario(id,nome,email,login,senha) values (2,'Admin','admin@gmail.com','admin','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
-insert into userusuario(id,nome,email,login,senha) values (3,'User','user@gmail.com','user','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
+insert into usuario(id,nome,email,login,senha) values (3,'User','user@gmail.com','user','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
 
 
 insert into user_roles(user_id,role_id) values(1, 1);
